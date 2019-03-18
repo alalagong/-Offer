730ffdce904dddcfddd281811d4819e70bf6d7cb
gong <gongyiqun51237@163.com>
2019-03-18 16:37:12 +0800

    delete build

--- 从尾到头打印链表/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gong/Project/C++/剑指Offer/从尾到头打印链表
-# Build directory: /home/gong/Project/C++/剑指Offer/从尾到头打印链表/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
