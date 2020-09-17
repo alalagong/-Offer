35065a069e4aa8ec33e0c9905ae3bb1127265734
alalagong <gongyiqun51237@163.com>
2020-09-17 13:14:54 +0800

    init

--- LeetCode/309.最佳买卖股票时机含冷冻期.cpp
+++ LeetCode/309.最佳买卖股票时机含冷冻期.cpp
@@ -34,7 +34,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n == 0)
+            return 0;
+        
+        vector<vector<int>> dp(n+2, vector<int>(2, 0)) ;
+        dp[1][1] = INT_MIN;
+        for(int i = 2; i <= n+1; ++i)
+        {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i-2]);
+            dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i-2]);
+        }
 
+        return dp[n+1][0];
     }
 };
 // @lc code=end
